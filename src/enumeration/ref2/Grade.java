99d8a7156ef9f75ed93ab7cceb483e1485dc3cee
master <ted3131@diquest.com>
2025-09-02 08:41:05 +0900

    commit

--- src/enumeration/ref2/Grade.java
+++ src/enumeration/ref2/Grade.java
@@ -1,5 +1,6 @@
 package enumeration.ref2;
 
+//enum 활용
 public enum Grade {
     BASIC(10), GOLD(20), DIAMOND(30);
 
--- src/nested/nested/NestedOuter.java
+++ src/nested/nested/NestedOuter.java
@@ -16,6 +16,7 @@ public class NestedOuter {
             //System.out.println(outInstanceValue);
 
             // 바깥 클래스의 클래스 멤버에는 접근할 수 있다. private도 접근 가능
+            System.out.println(outClassValue);
             System.out.println(NestedOuter.outClassValue);
         }
     }
